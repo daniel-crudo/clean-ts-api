34e34b7b407baf69f092a1c27b8b8531739faca1
Crud0Dev <daniel.crudo3@gmail.com>
2022-04-08 13:38:33 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,16 +1,16 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
-
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
   test('Should return an account on success', async () => {
-    const sut = new AccountRepository()
+    const sut = new AccountMongoRepository()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
