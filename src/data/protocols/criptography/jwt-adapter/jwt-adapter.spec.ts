575b5186f63031afb149ab6da5c1280f10bde539
Crud0Dev <daniel.crudo3@gmail.com>
2022-06-17 16:40:26 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/data/protocols/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/data/protocols/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -18,6 +18,15 @@ describe('Jwt Adapter', () => {
   test('Should return a token on sign success', async () => {
     const sut = new JwtAdapter('secret')
     const accessToken = await sut.encrypt('any_id')
-    expect(accessToken).toBe('')
+    expect(accessToken).toBe('any_token')
+  })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
