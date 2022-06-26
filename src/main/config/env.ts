f86562fb38bd17aec87247fb63bbf6564f03b00d
Crud0Dev <daniel.crudo3@gmail.com>
2022-06-26 11:25:36 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-ddd',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '964276AA5FA041C1F390861A3A28021E89048216A7E566D6AACA1A10FBA9EFF0'
 }
