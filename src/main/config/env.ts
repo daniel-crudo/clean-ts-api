eec0849474198361a7215169918f9bff2f384867
Silva, Daniel Augusto <daniel.augusto-ext@ab-inbev.com>
2022-08-31 20:06:22 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-ddd',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-ts-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '964276AA5FA041C1F390861A3A28021E89048216A7E566D6AACA1A10FBA9EFF0'
 }
